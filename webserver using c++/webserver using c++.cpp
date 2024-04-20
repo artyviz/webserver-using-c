9e4ae04e7236449a112977ec49e91bc68e1216fd
umar farhan <umarfarhan474@gmail.com>
2024-04-21 03:14:26 +0530

    initialization done

---
+++ webserver using c++/webserver using c++.cpp
@@ -0,0 +1,23 @@
+#define _WINSOCK_DEPRECTAED_NO_WARNINGS
+#pragma comment(lib , "ws2_32.lib")
+#include <iostream>
+#include <WinSock2.h>
+#include <string>
+int main()
+{
+	std::cout << "Attempting to create a server";
+	
+	SOCKET wsocket;
+	SOCKET new_wsockety;
+	WSADATA wsaData;
+	struct sockaddr_in server;
+	int server_len;
+	int BUFFER_SIZE = 37020;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		std::cout << "couldn't initialize";
+	}
+	
+	return 0;
+}
