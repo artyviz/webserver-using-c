7b95ad77a1154d93998075ff339f5ae2d782e774
umar farhan <umarfarhan474@gmail.com>
2024-04-21 14:09:51 +0530

    read request of client

--- webserver using c++/webserver using c++.cpp
+++ webserver using c++/webserver using c++.cpp
@@ -8,7 +8,7 @@ int main()
 	std::cout << "Attempting to create a server";
 	
 	SOCKET wsocket;
-	SOCKET new_wsockety;
+	SOCKET new_wsocket;
 	WSADATA wsaData;
 	struct sockaddr_in server;
 	int server_len;
@@ -25,5 +25,39 @@ int main()
 		std::cout << "Coudln't create a socket";
 	}
 
-	return 0;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_port = htons(8080);
+	server_len = sizeof(server);
+
+	if (bind(wsocket, (SOCKADDR*)&server, server_len) != 0)
+	{
+		std::cout << "unable to bind the socket";
+	}
+
+	if (listen(wsocket, 20) != 0)
+	{
+		std::cout << "couldn't start  listening";
+	}
+
+	std::cout << "listening on 127.0.0.1:8080\n";
+
+	int bytes = 0;
+	while (true)
+	{
+		new_wsocket = accept(wsocket, (SOCKADDR*)&server, &server_len);
+		if (new_wsocket == INVALID_SOCKET)
+		{
+			std::cout << "couldn't accept \n";
+		}
+		
+		char buff[30720] = { 0 };
+		bytes = recv(new_wsocket, buff, BUFFER_SIZE, 0);
+		if (bytes < 0)
+		{
+			std::cout << "couldn't read client request";
+		}
+	}
+
+	return 0; 
 }
